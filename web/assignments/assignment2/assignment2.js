94907c603deec959f1815192b5fe2f090c3e1510
unini <unini056378@gmail.com>
2017-01-16 00:50:06 +0900

    Change assignment2.js

--- web/assignments/assignment2/assignment2.js
+++ web/assignments/assignment2/assignment2.js
@@ -1,17 +1,3 @@
-/* 
- * 1. executeItemNode 함수를 완성하세요
- * 이 함수의 actionType 매개변수는 'add' 또는 'remove'를 받습니다.  add를 받으면 추가하고, remove를 받으면 일을 삭제합니다.
- * todoORNumber 는 add일때는 새로운 일을 문자열로 받고, remove일때는 숫자를 받습니다.
- * 할일 목록은 할일의 문자열 길이 순으로 정렬됩니다. 목록이 추가될때마다 바로 정렬되야 합니다.(가장 긴 할일 내용이 뒤로 가야함)
- * 삭제하려는 경우 num과 일치하는 item번호가 없다면 'message' 영역에서 적당한 메시지를 붉은색으로 표시됐다 3초뒤 사라집니다.
- * 추가하려는 경우 이미 같은일이 있다면 message영역에서 적당한 메시지를 붉은색으로 표시했다 3초뒤 사라집니다.
- * 함수를 여러개로 나눠도 상관없습니다.
- * 참고로 1번을 풀기 위해서는 string조작과 setTimeout이라는 것을 공부해야 할 수도 있습니다.
- * 
- * 2. 좀더 사용하기 쉬운 웹화면이 되도록, css에 다양한 스타일을 적용하면서 꾸며봅니다.
- * 
- * 3. 아래 event 관련 코드를 학습해보고, 어떤 코드를 의미하는지 최대한 자세히 주석으로 설명을 넣어보세요.
- */
 
 const listSection = document.querySelector(".basket > ul");
 const messageSection = document.querySelector(".message");
@@ -72,7 +58,6 @@ function _appendNewChildNode(root, todo) {
   li.appendChild(document.createTextNode(todo));
   root.appendChild(li);
 }
-
 
 /*
  querySelector를 이용해서 얻어진 controller에  onclick함수에  callback함수를 매개변수로 넣어 addEventListener함수를 호출합니다
